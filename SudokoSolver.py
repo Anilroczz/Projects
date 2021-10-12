32e060c9044cf1074686654bf1742d1a4517a0ea
Anil Kumar <68889621+Anilroczz@users.noreply.github.com>
2021-10-12 23:58:10 +0530

    Update SudokoSolver.py

--- SudokoSolver.py
+++ SudokoSolver.py
@@ -58,18 +58,7 @@ def sudokoSolver(board,row,col) :
     # if a grid doesn't satisfy any of the above conditions , it implies that the grid is non-solvable.
     return False
 
-
-
-boardx = [[0, 7, 2, 0, 0, 4, 0, 5, 3],
-          [0, 4, 9, 0, 1, 0, 0, 8, 2],
-          [8, 0, 0, 2, 5, 7, 9, 6, 0],
-          [0, 0, 0, 7, 4, 0, 0, 0, 0],
-          [0, 0, 7, 8, 0, 0, 6, 4, 9],
-          [0, 8, 4, 0, 9, 0, 3, 0, 5],
-          [9, 2, 0, 0, 6, 1, 0, 3, 0],
-          [0, 0, 0, 4, 0, 0, 0, 1, 0],
-          [0, 0, 0, 3, 7, 8, 0, 9, 0]]
-
+#example checking problem.
 board =  [[2, 5, 0, 0, 3, 0, 9, 0, 1],
           [0, 1, 0, 0, 0, 4, 0, 0, 0],
           [4, 0, 7, 0, 0, 0, 2, 0, 8],
